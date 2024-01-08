b48f5b41e61905694c1e0e129110192ca699cd2b
Yurii Bondarenko <yuri.gr.bond@gmail.com>
2024-01-08 20:17:32 +0100

    Added AssertableResponse

--- src/services.py
+++ src/services.py
@@ -1,7 +1,9 @@
-import json
 import os
-import requests
+
 import allure
+import requests
+
+from src.response import AssertableResponse
 
 
 class ApiService(object):
@@ -31,34 +33,34 @@ class UserApiService(ApiService):
 
     @allure.step
     def get_user_current(self, headers):
-        return self._get(endpoint="/users/current", headers=headers)
+        return AssertableResponse(self._get(endpoint="/users/current", headers=headers))
 
     @allure.step
     def get_user_profile(self, headers):
-        return self._get(endpoint="/users/profile", headers=headers)
+        return AssertableResponse(self._get(endpoint="/users/profile", headers=headers))
 
     def get_user_settings(self, headers):
-        return self._get(endpoint="/users/settings", headers=headers)
+        return AssertableResponse(self._get(endpoint="/users/settings", headers=headers))
 
     @allure.step
     def edit_user_profile(self, body, headers):
-        return self._put(endpoint="/users/profile", body=body, headers=headers)
+        return AssertableResponse(self._put(endpoint="/users/profile", body=body, headers=headers))
 
     @allure.step
     def edit_user_settings(self, body, headers):
-        return self._put(endpoint="/users/settings", body=body, headers=headers)
+        return AssertableResponse(self._put(endpoint="/users/settings", body=body, headers=headers))
 
     @allure.step
     def change_user_email(self, body, headers):
-        return self._put(endpoint="/users/email", body=body, headers=headers)
+        return AssertableResponse(self._put(endpoint="/users/email", body=body, headers=headers))
 
     @allure.step
     def change_user_password(self, body, headers):
-        return self._put(endpoint="/users/password", body=body, headers=headers)
+        return AssertableResponse(self._put(endpoint="/users/password", body=body, headers=headers))
 
     @allure.step
     def delete_user(self, headers):
-        return self._delete(endpoint="/users", headers=headers)
+        return AssertableResponse(self._delete(endpoint="/users", headers=headers))
 
 
 class CarApiService(ApiService):
@@ -67,32 +69,32 @@ class CarApiService(ApiService):
 
     @allure.step
     def create_new_car(self, body, headers):
-        return self._post(endpoint="/cars", body=body, headers=headers)
+        return AssertableResponse(self._post(endpoint="/cars", body=body, headers=headers))
 
     @allure.step
     def get_car_brand(self, headers):
-        return self._get(endpoint="/cars/brands", headers=headers)
+        return AssertableResponse(self._get(endpoint="/cars/brands", headers=headers))
 
     @allure.step
     def get_car_brand_by_id(self, headers, random):
-        return self._get(endpoint=f"/cars/brands/{random}", headers=headers)
+        return AssertableResponse(self._get(endpoint=f"/cars/brands/{random}", headers=headers))
 
     @allure.step
     def get_cars_model(self, headers):
-        return self._get(endpoint="/cars/models", headers=headers)
+        return AssertableResponse(self._get(endpoint="/cars/models", headers=headers))
 
     @allure.step
     def get_car_model_by_id(self, headers, random):
-        return self._get(endpoint=f"/cars/models/{random}", headers=headers)
+        return AssertableResponse(self._get(endpoint=f"/cars/models/{random}", headers=headers))
 
     @allure.step
     def get_current_user_car_by_id(self, headers, created_car_id):
-        return self._get(endpoint=f"/cars/{created_car_id}", headers=headers)
+        return AssertableResponse(self._get(endpoint=f"/cars/{created_car_id}", headers=headers))
 
     @allure.step
     def edit_existing_car(self, body, headers, created_car_id):
-        return self._put(endpoint=f"/cars/{created_car_id}", body=body, headers=headers)
+        return AssertableResponse(self._put(endpoint=f"/cars/{created_car_id}", body=body, headers=headers))
 
     @allure.step
     def delete_existing_car(self, headers, created_car_id):
-        return self._delete(endpoint=f"/cars/{created_car_id}", headers=headers)
+        return AssertableResponse(self._delete(endpoint=f"/cars/{created_car_id}", headers=headers))
--- tests/test_users.py
+++ tests/test_users.py
@@ -6,33 +6,30 @@ user_api = UserApiService()
 
 def test_get_authenticated_user_data(sign_up_response, headers):
     response = user_api.get_user_current(headers=headers)
-    response_json = response.json()
 
-    assert response.status_code == 200, 'Status code broken'
-    assert response_json['status'] == 'ok'
-    assert response_json['data']['userId'] == sign_up_response.json()['data']['userId']
+    assert response.is_status_code(200)
+    assert response.get_field('status') == 'ok'
+    assert response.get_field('data')['userId'] == sign_up_response.json()['data']['userId']
 
 
 def test_get_authenticated_user_profile_data(sign_up_response, headers):
     response = user_api.get_user_profile(headers=headers)
-    response_json = response.json()
     sign_up_response_request_body = json.loads(sign_up_response.request.body)
 
-    assert response.status_code == 200, 'Status code broken'
-    assert response_json['status'] == 'ok'
-    assert response_json['data']['userId'] == sign_up_response.json()['data']['userId']
-    assert response_json['data']['name'] == sign_up_response_request_body['name']
-    assert response_json['data']['lastName'] == sign_up_response_request_body['lastName']
+    assert response.is_status_code(200)
+    assert response.get_field('status') == 'ok'
+    assert response.get_field('data')['userId'] == sign_up_response.json()['data']['userId']
+    assert response.get_field('data')['name'] == sign_up_response_request_body['name']
+    assert response.get_field('data')['lastName'] == sign_up_response_request_body['lastName']
 
 
 def test_get_authenticated_user_settings_data(sign_up_response, headers):
     response = user_api.get_user_settings(headers=headers)
-    response_json = response.json()
 
-    assert response.status_code == 200, 'Status code broken'
-    assert response_json['status'] == 'ok'
-    assert response_json['data']['currency'] == sign_up_response.json()['data']['currency']
-    assert response_json['data']['distanceUnits'] == sign_up_response.json()['data']['distanceUnits']
+    assert response.is_status_code(200)
+    assert response.get_field('status') == 'ok'
+    assert response.get_field('data')['currency'] == sign_up_response.json()['data']['currency']
+    assert response.get_field('data')['distanceUnits'] == sign_up_response.json()['data']['distanceUnits']
 
 
 def test_edit_user_profile(sign_up_response, headers):
@@ -45,18 +42,17 @@ def test_edit_user_profile(sign_up_response, headers):
     })
     headers['Content-Type'] = 'application/json'
     response = user_api.edit_user_profile(body=payload, headers=headers)
-    response_json = response.json()
     payload_json = json.loads(payload)
     sign_up_response_request_body = json.loads(sign_up_response.request.body)
 
-    assert response.status_code == 200, 'Status code broken'
-    assert response_json['status'] == 'ok'
-    assert response_json['data']['userId'] == sign_up_response.json()['data']['userId']
-    assert response_json['data']['photoFilename'] == sign_up_response.json()['data']['photoFilename']
-    assert response_json['data']['name'] == sign_up_response_request_body['name']
-    assert response_json['data']['lastName'] == sign_up_response_request_body['lastName']
-    assert response_json['data']['dateBirth'] == payload_json['dateBirth']
-    assert response_json['data']['country'] == payload_json['country']
+    assert response.is_status_code(200)
+    assert response.get_field('status') == 'ok'
+    assert response.get_field('data')['userId'] == sign_up_response.json()['data']['userId']
+    assert response.get_field('data')['photoFilename'] == sign_up_response.json()['data']['photoFilename']
+    assert response.get_field('data')['name'] == sign_up_response_request_body['name']
+    assert response.get_field('data')['lastName'] == sign_up_response_request_body['lastName']
+    assert response.get_field('data')['dateBirth'] == payload_json['dateBirth']
+    assert response.get_field('data')['country'] == payload_json['country']
 
 
 def test_edits_users_settings(sign_up_response, headers):
@@ -66,13 +62,12 @@ def test_edits_users_settings(sign_up_response, headers):
     })
     headers['Content-Type'] = 'application/json'
     response = user_api.edit_user_settings(body=payload, headers=headers)
-    response_json = response.json()
     payload_json = json.loads(payload)
 
-    assert response.status_code == 200, 'Status code broken'
-    assert response_json['status'] == 'ok'
-    assert response_json['data']['currency'] == payload_json['currency']
-    assert response_json['data']['distanceUnits'] == payload_json['distanceUnits']
+    assert response.is_status_code(200)
+    assert response.get_field('status') == 'ok'
+    assert response.get_field('data')['currency'] == payload_json['currency']
+    assert response.get_field('data')['distanceUnits'] == payload_json['distanceUnits']
 
 
 def test_changes_users_email(sign_up_response, headers):
@@ -84,11 +79,10 @@ def test_changes_users_email(sign_up_response, headers):
     })
     headers['Content-Type'] = 'application/json'
     response = user_api.change_user_email(body=payload, headers=headers)
-    response_json = response.json()
 
-    assert response.status_code == 200, 'Status code broken'
-    assert response_json['status'] == 'ok'
-    assert response_json['data']['userId'] == sign_up_response.json()['data']['userId']
+    assert response.is_status_code(200)
+    assert response.get_field('status') == 'ok'
+    assert response.get_field('data')['userId'] == sign_up_response.json()['data']['userId']
 
 
 def test_changes_users_password(url, sign_up_response, headers):
@@ -103,16 +97,14 @@ def test_changes_users_password(url, sign_up_response, headers):
     })
     headers['Content-Type'] = 'application/json'
     response = user_api.change_user_password(body=payload, headers=headers)
-    response_json = response.json()
 
-    assert response.status_code == 200, 'Status code broken'
-    assert response_json['status'] == 'ok'
-    assert response_json['data']['userId'] == sign_up_response.json()['data']['userId']
+    assert response.is_status_code(200)
+    assert response.get_field('status') == 'ok'
+    assert response.get_field('data')['userId'] == sign_up_response.json()['data']['userId']
 
 
 def test_delete_users_account_and_session(sign_up_response, headers):
     response = user_api.delete_user(headers=headers)
-    response_json = response.json()
 
-    assert response.status_code == 200, 'Status code broken'
-    assert response_json['status'] == 'ok'
+    assert response.is_status_code(200)
+    assert response.get_field('status') == 'ok'
